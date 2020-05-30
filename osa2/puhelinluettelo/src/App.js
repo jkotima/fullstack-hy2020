cd650c7cc2e4061a21158c9680c33d126fb9f8af
jkotima <jkotima@gmail.com>
2020-05-30 14:29:52 +0300

    tehtävät 2.6.-2.10.

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react'
+import Person from './components/Person'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+
+const App = () => {
+    const [ persons, setPersons] = useState([
+      { 
+        name: 'Arto Hellas',
+        number: '666'
+      },
+      { name: 'Ada Lovelace', number: '39-44-5323523' },
+      { name: 'Dan Abramov', number: '12-43-234345' },
+      { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ]) 
+    
+    const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
+    const [ filter, setFilter ] = useState('')
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName,
+            number: newNumber
+        }
+        if (persons.map((person) => person.name).includes(newName)) {
+            alert(`${newName} is already added to phonebook`)
+        } else {
+            setPersons(persons.concat(personObject))
+            setNewName('')
+            setNewNumber('')
+        }
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+    }
+
+    const personsToShow = persons.filter(person => 
+        person.name.toUpperCase().includes(filter.toUpperCase())
+    )
+
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Filter value={filter} onChange={handleFilterChange}/>
+
+        <h2>Add a new</h2>
+        <PersonForm 
+            addPerson={addPerson}
+            newName={newName}
+            newNumber={newNumber}
+            handleNameChange={handleNameChange}
+            handleNumberChange={handleNumberChange}
+        />
+
+        <h2>Numbers</h2>
+        <Persons persons={personsToShow}/>
+      </div>
+    )
+}
+
+export default App
